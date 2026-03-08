e460e2440b336c10e8727ab7573e45e850fa2995
Raghvendra Mishra <mishra.raghvendra@gmail.com>
2026-03-07 21:43:25 -0500

    remove

--- 05_src/assignment_chat/chatbot/server/summary.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-class summary(BaseModel):
-    Author: str
-    Title: str
-    Relevance: str
-    Summary: str
-    Tone: str
-    Input_Tokens: int
-    Output_Tokens: int
